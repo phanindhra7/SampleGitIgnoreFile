5e2ec69931fe0ae6fb387799348c0d8ae0eae067
phanindhra7 <phanindra.bandaru@smscountry.com>
2019-07-03 16:38:18 +0530

    Sample git folder added

---
+++ SampleGit/SampleGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleGit
+//
+//  Created by SMSC-PMacmini on 03/07/19.
+//  Copyright © 2019 phanindhra B V. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
